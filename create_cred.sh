fa7247c65d8b66fa27dab9e5fca9918e49143a9c
suboty <seed.suboty@gmail.com>
2024-12-02 19:26:41 +0300

    add scripts for regex101 parsing

---
+++ create_cred.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Prepare regex101..."
+sh src/parse_regex101.sh
--- src/parse_regex101.sh
+++ src/parse_regex101.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # parse all pages MOST_POINTS
-#sh regex101/parse_pages.sh
+sh src/regex101/parse_pages.sh
 
 # extract individuals from pages
-#sh regex101/extract_individuals.sh
+sh src/regex101/extract_individuals.sh
+
+# migrate regexes to db
+python3 src/regex101/migrate_regexes.py
