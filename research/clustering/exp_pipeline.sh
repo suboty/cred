6af0230333300739463ed9db0ac642989ef5096f
seed.suboty <seed.suboty@gmail.com>
2025-03-07 11:46:19 +0300

    add shell script for experiments

---
+++ research/clustering/exp_pipeline.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python_activate() {
+  . "../../.venv/bin/activate" || exit
+}
+
+python_activate
+
+str="="
+lineStr=$(printf "$str%.0s" {1..10})
+
+for arg in "$@"; do
+    echo "Experiment with <$arg> filter word"
+	python3 clustering.py -v -u --algname tf_idf --filter $arg
+	python3 clustering.py -v -u --algname bert --filter $arg
+	echo "$lineStr"
+done
