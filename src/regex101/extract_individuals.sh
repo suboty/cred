3e730b50f1e0986ce61c3dc28d8a622081e09f00
seed.suboty <seed.suboty@gmail.com>
2025-03-27 11:26:32 +0300

    fix iteration on regexes from regex101

--- src/regex101/extract_individuals.sh
+++ src/regex101/extract_individuals.sh
@@ -6,8 +6,8 @@ COUNTER=0
 jq -cr '.data[] | (.permalinkFragment + " https://regex101.com/api/regex/" + .permalinkFragment + "/" + (.version | tostring))' regex101/pages/*.json | \
     while read -r frag url;
     do
-        COUNTER=$[$COUNTER +1]
-        printf "$COUNTER | "
+        COUNTER=$((COUNTER+1))
+        echo "$COUNTER | "
         # If the regex has not already been fetched, fetch it
         [ -f "regex101/regexes/$frag.json" ] || (wget -nv -O "regex101/regexes/$frag.json" -nc "$url"; sleep 1)
     done
