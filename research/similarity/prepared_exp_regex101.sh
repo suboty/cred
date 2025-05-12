83a5b2c00e954b3027df537488821e090874543f
seed.suboty <seed.suboty@gmail.com>
2025-05-12 13:45:08 +0300

    add prepared experiment for regex101

--- research/similarity/prepared_exp_regex101.sh
+++ research/similarity/prepared_exp_regex101.sh
@@ -13,12 +13,11 @@ python_activate
 clear_old_results
 
 ! [ -f "../../regex101.db" ] && { echo "regex101.db is not found!"; exit; }
-! [ -f "../../regexlib.db" ] && { echo "regexlib.db is not found!"; exit; }
 
-regexSource="regexlib"
+regexSource="regex101"
 regexConstruction="[0-9]"
-regexConstructionThresholdLower="0.3"
-regexConstructionThresholdUpper="0.5"
+regexConstructionThresholdLower="0.1"
+regexConstructionThresholdUpper="0.2"
 
 python3 similarity.py \
 --regexGroup "same_construction_percentage" \
---
+++ research/similarity/prepared_exp_regexlib.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+python_activate() {
+  . "../../.venv/bin/activate" || exit
+}
+
+clear_old_results() {
+	! [ -d results ] || sudo rm -r results
+}
+
+python_activate
+
+clear_old_results
+
+! [ -f "../../regexlib.db" ] && { echo "regexlib.db is not found!"; exit; }
+
+regexSource="regexlib"
+regexConstruction="[0-9]"
+regexConstructionThresholdLower="0.3"
+regexConstructionThresholdUpper="0.5"
+
+python3 similarity.py \
+--regexGroup "same_construction_percentage" \
+--regexConstruction "$regexConstruction" \
+--regexSource "$regexSource" \
+--regexConstructionThresholdLower "$regexConstructionThresholdLower" \
+--regexConstructionThresholdUpper "$regexConstructionThresholdUpper"
