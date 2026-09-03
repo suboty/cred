b2e34e83f988a10c324c9590147530acfb2fd2d8
n.moroshkin <seed.suboty@gmail.com>
2026-09-03 15:38:56 +0300

    remove old files for fully refactoring

--- create_cred.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Prepare regex101..."
-sh src/parse_regex101.sh
-
-echo "Prepare regexlib..."
-sh src/parse_regexlib.sh
--- src/parse_regexlib.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# parse and migrate regexes to db
-python3 src/regexlib/parse_lib.py
